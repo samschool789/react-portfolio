1d94938e5afbc328bfcf0e90b13bf3ca053c55bb
samschool789@gmail.com <samschool789@gmail.com>
2022-08-01 14:53:56 -0500

    inital commit

---
+++ src/resumeData.js
@@ -0,0 +1,92 @@
+let resumeData = {
+    "imagebaseurl":"https://samschool789.github.io/",
+    "name": "Sam Mcilvoy",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Sam Mcilvoy",
+    
+    "roleDescription": "I enjoy creating the frontend of applications.  Constantly searching for the perfect user experience.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/samuel-mcilvoy-b0a438234/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/samschool789",
+          "className":"fa fa-github"
+        },
+        {
+            "name":"resume",
+            "url": "https://docs.google.com/document/d/1eRAZBPtDD4YKmi0iOaAHk67WfnywQlEroUSEX05Elyk/edit",
+            "className":"fa fa-resume"
+        }
+      
+      ],
+    "aboutme":"I am currently a full stack development student through the University of Kansas.  I will have my certificate in August of the year 2022.  Very confident and excited to further my developer journey.",
+    "address":"Kansas City, MO",
+    "website":"https://samschool789.github.io",
+    "resume": "https://docs.google.com/document/d/1eRAZBPtDD4YKmi0iOaAHk67WfnywQlEroUSEX05Elyk/edit",
+    "education":[
+      {
+        "UniversityName":"University of Kansas",
+        "specialization":"Full stack web developer.",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2022",
+        "Achievements":"Our project won in every category after they were presented.  The whole class voted and we won by a landslide in every category!"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"full stack application",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"landing page",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"node js app",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+  
+  }
+  
+  export default resumeData
